e92019df3f5883ae77091e359f8e4d9e298fdef0
Ravi Jaganathan <raviregi@gmail.com>
2020-03-02 02:55:16 +0000

    more data added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,10 @@ insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (1, 'Harry','Pot
 insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (2, 'Ronald','Weasley','Bilius');
 insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (3, 'Hermione','Granger','Granger');
 insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (4, 'Albus','Dumbledore','Percival Wulfric Brian');
+insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (5, 'Lord ','Voldemort','');
+insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (6, 'Severus','Snape','');
+insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (7, 'Sirius','Black','');
+insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (8, 'Minerva','McGonagall','');
+insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (9, 'Ginny','Weasley','');
+insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (10, 'Neville','Longbottom','');
+insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (11, 'Bellatrix','Lestrange','');
