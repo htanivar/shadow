aecba270693a6c731f859474c8389c73d12fc9b2
htanivar <raviregi@gmail.com>
2020-10-04 16:13:42 +0100

    deleted data.sql

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (1, 'Harry','Potter','James');
-insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (2, 'Ronald','Weasley','Bilius');
-insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (3, 'Hermione','Granger','Granger');
-insert into CHARACTER (ID,FIRSTNAME,LASTNAME,MIDDLENAME) values (4, 'Albus','Dumbledore','Percival Wulfric Brian');
